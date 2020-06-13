a46f72a11e1fb00d7578ab60129f84459af5dad1
[ Adway Gupta - Physics Department - Graduate Student - asing250 ] <agupt256@agave2.agave.rc.asu.edu>
2020-06-12 19:05:29 -0700

    Latest version of the querying code

---
+++ Code/submit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#SBATCH -n 1                        # number of cores
+#SBATCH -t 0-4:00                  # wall time (D-HH:MM)
+#SBATCH -o slurm.%j.out             # STDOUT (%j = JobId)
+
+python3 query_test_4.py
